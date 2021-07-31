8d50fb73b2392d793167c54f5d19923738bbeffa
zhousujuan <179997445@qq.com>
2021-07-31 10:24:59 +0800

    修改src->api->user.js内部的所有请求地址为接口文档中的真实地址

--- src/api/user.js
+++ src/api/user.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function login(data) {
   return request({
-    url: '/vue-admin-template/user/login',
+    url: '/admin/acl/index/login',
     method: 'post',
     data
   })
@@ -10,15 +10,19 @@ export function login(data) {
 
 export function getInfo(token) {
   return request({
-    url: '/vue-admin-template/user/info',
+    url: '/admin/acl/index/info',
     method: 'get',
-    params: { token }
+    // 这里改写了？？？还有注释的意思
+    // 此处的params其实是用来接收query传参用的,会自动将对象的属性名作为key,属性值作为value
+    params: {
+      token:token
+     }
   })
 }
 
 export function logout() {
   return request({
-    url: '/vue-admin-template/user/logout',
+    url: '/admin/acl/index/logout',
     method: 'post'
   })
 }
