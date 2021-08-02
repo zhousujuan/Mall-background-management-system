f7e0cd2ad4bed4083dd0cd6d41d038d979499915
zhousujuan <179997445@qq.com>
2021-08-02 09:39:19 +0800

    '完成品牌添加功能的静态样式搭建'

--- src/views/shop/brand/index.vue
+++ src/views/shop/brand/index.vue
@@ -14,7 +14,29 @@
       </div>
       <!-- 右边添加品牌部分 -->
       <div class="addBrand">
-        <el-button type="primary" icon="el-icon-plus">添加</el-button>
+        <el-button type="primary" icon="el-icon-plus" @click="dialogFormVisible = true">添加</el-button>
+        <el-dialog title="添加品牌" :visible.sync="dialogFormVisible">
+          <el-form :model="form">
+            <el-form-item label="品牌名称：" :label-width="formLabelWidth">
+              <el-input v-model="form.name" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="品牌LOGO：" :label-width="formLabelWidth">
+              <el-upload
+                class="avatar-uploader"
+                action="https://jsonplaceholder.typicode.com/posts/"
+                :show-file-list="false"
+                :on-success="handleAvatarSuccess"
+                :before-upload="beforeAvatarUpload">
+                <img v-if="imageUrl" :src="imageUrl" class="avatar">
+                <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+              </el-upload>
+            </el-form-item>
+          </el-form>
+          <div slot="footer" class="dialog-footer">
+            <el-button @click="dialogFormVisible = false">取 消</el-button>
+            <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+          </div>
+        </el-dialog>
       </div>
     </div>
     <!-- 中间表格品牌数据列表部分 -->
@@ -51,10 +73,11 @@
           size="mini"
           type="warning"
           @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+
         <el-button
           size="mini"
           type="danger"
-          @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+          @click="handleDelete(scope.row)">删除</el-button>
       </template>
     </el-table-column>
     </el-table-column>
@@ -82,14 +105,31 @@ export default {
   name: "Brand",
   data() {
     return {
+      //搜索功能
       searchName: "",
       searchList: [],
-
+      //品牌列表
       brandList: [],
 
+      //分页器的
       currentPage: 1,
       limit: 3,
       total: 15,
+      //添加功能
+      dialogFormVisible: true,
+      form: {
+          name: '',
+          region: '',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: ''
+        },
+        formLabelWidth: '150px',
+        //上传的logo
+        imageUrl: ''
     };
   },
   mounted() {
@@ -133,51 +173,75 @@ export default {
     //获取搜索结果列表
     getSearchList(val) {},
 
-    //编辑
-    handleEdit(index, row) {
-      console.log(index, row);
+    //编辑功能
+    handleEdit(row) {
+      console.log(row);
+      const h = this.$createElement;
     },
     //删除操作
-    handleDelete(index, row) {
-      console.log(index, row);
-      this.$confirm(`此操作将永久删除该品牌:"${row.tmName}", 是否继续?`, '提示', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-          type: 'warning'
-        }).then( async() => {
+    handleDelete(row) {
+      console.log(row);
+      this.$confirm(
+        `此操作将永久删除该品牌:  "${row.tmName}", 是否继续?`,
+        "提示",
+        {
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          type: "warning",
+        }
+      )
+        .then(async () => {
           //点击确认
-          try{
+          try {
             //1.发送请求删除该品牌
             await this.$API.brand.deleteBrand(row.id);
 
             //2.弹窗提示用户删除成功
             this.$message({
-              type: 'success',
-              message: '删除成功!'
-            })
-
-          }catch(error){
+              type: "success",
+              message: "删除成功!",
+            });
+          } catch (error) {
             //2.弹窗提示用户删除失败
             this.$message({
-            type: 'info',
-            message: '删除失败!'
-          })
+              type: "info",
+              message: "删除失败!",
+            });
           }
           //3.请求最新列表,并展示
-            //3.1如果当前页面删除了该数据,还有数据需要展示,就请求当前页面
-            //3.2如果当前页面删除了该数据,就没有数据了,就请求上一页
-            // console.log(1,this.page)
-            this.getBrandList(this.brandList.length>1?this.currentPage:this.currentPage-1);
-
-        }).catch(() => {
+          //3.1如果当前页面删除了该数据,还有数据需要展示,就请求当前页面
+          //3.2如果当前页面删除了该数据,就没有数据了,就请求上一页
+          // console.log(1,this.page)
+          this.getBrandList(
+            this.brandList.length > 1 ? this.currentPage : this.currentPage - 1
+          );
+        })
+        .catch(() => {
           // 如果用户点击取消按钮,会执行.catch内部的代码
           this.$message({
-            type: 'info',
-            message: '已取消删除'
+            type: "info",
+            message: "已取消删除",
           });
         });
+    },
+
+    //上传logo
+    handleAvatarSuccess(res, file) {
+        this.imageUrl = URL.createObjectURL(file.raw);
+      },
+      beforeAvatarUpload(file) {
+        const isJPG = file.type === 'image/jpeg';
+        const isLt2M = file.size / 1024 / 1024 < 2;
+
+        if (!isJPG) {
+          this.$message.error('上传头像图片只能是 JPG 格式!');
+        }
+        if (!isLt2M) {
+          this.$message.error('上传头像图片大小不能超过 2MB!');
+        }
+        return isJPG && isLt2M;
       }
-  }
+  },
 };
 </script>
 
@@ -205,5 +269,33 @@ export default {
     margin-top: 20px;
     text-align: center;
   }
+
+  //上传的图片样式
+  .avatar-uploader{
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+    width: 178px;
+    height: 178px;
+    .el-upload:hover {
+      border-color: #409EFF;
+    }
+    .avatar-uploader-icon {
+      font-size: 28px;
+      color: #8c939d;
+      width: 178px;
+      height: 178px;
+      line-height: 178px;
+      text-align: center;
+    }
+    .avatar {
+      width: 178px;
+      height: 178px;
+      display: block;
+    }
+
+  }  
 }
 </style>
